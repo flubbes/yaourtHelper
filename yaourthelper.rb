a777387e512682d684c0798aa9f2f6646f12dde1
Fabian Berg <info@fabian-berg.de>
2015-09-17 18:06:41 +0200

    script now opens google searches for all new package versions

--- yaourthelper.rb
+++ yaourthelper.rb
@@ -1,1 +1,26 @@
 #!/usr/bin/ruby
+require 'launchy'
+require 'open-uri'
+
+#detect yaourt
+isYaourtInstalled = File.exists? '/usr/bin/yaourt'
+
+if !isYaourtInstalled
+	puts 'yaourt is not installed.'
+	Process.exit
+end
+
+#check for updates
+yaourt = open('|sudo yaourt -Qua')
+out = yaourt.readlines
+yaourt.close
+out.each do |line|
+	upgrade = line.split(' ')
+	packageName = upgrade[0].split('/')[1]
+	newVersion = upgrade[-1].split('-')[0..-2] * ""
+	url = 'https://google.com/#q='
+	url << URI::encode(packageName)
+	url << URI::encode(' release notes ')
+	url << URI::encode(newVersion)
+	Launchy.open(url)
+end
