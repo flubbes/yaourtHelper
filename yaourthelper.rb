18f4f1f0e59e1ac96fbcec1cf249aa54f418a696
Fabian Berg <info@fabian-berg.de>
2015-09-17 18:53:59 +0200

    added search engine uri as settings variable

--- yaourthelper.rb
+++ yaourthelper.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/ruby
+
 require 'launchy'
 require 'open-uri'
+searchEngineBaseUrl = 'https://google.com/#q='
+
+
+
 
 #detect yaourt
 isYaourtInstalled = File.exists? '/usr/bin/yaourt'
@@ -18,7 +23,7 @@ out.each do |line|
 	upgrade = line.split(' ')
 	packageName = upgrade[0].split('/')[1]
 	newVersion = upgrade[-1].split('-')[0..-2] * ""
-	url = 'https://google.com/#q='
+	url = searchEngineBaseUrl
 	url << URI::encode(packageName)
 	url << URI::encode(' release notes ')
 	url << URI::encode(newVersion)
